ec616c9d73ccd99e41445be3accd19ce2d97d57c
JUSTDEEPAN <120191409+JUSTDEEPAN@users.noreply.github.com>
2023-06-16 18:53:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song1 = "";
+song2 = "";
+
+function preload()
+{
+    song1 = loadSound("song 1.mp3");
+    song2 = loadSound("song 2 (1).mp3 ");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
